8fe4c6771efdb3e2c80f72e6e55a6111eacbe1da
username <useremail@ai.com>
2024-06-25 14:55:34 +0800

    添加

---
+++ index.905b2.js
@@ -0,0 +1,30 @@
+System.register(["./application.1c0f1.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, canvas, $p, bcr, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_application1c0f1Js) {
+      Application = _application1c0f1Js.Application;
+    }],
+    execute: function () {
+      canvas = document.getElementById('GameCanvas');
+      $p = canvas.parentElement;
+      bcr = $p.getBoundingClientRect();
+      canvas.width = bcr.width;
+      canvas.height = bcr.height;
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
